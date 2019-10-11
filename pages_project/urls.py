6d918cb928d587294aac7bbdc9939e253b6e0ec5
Dustin Squires <itsyourdustiny@gmail.com>
2019-10-11 16:10:20 -0400

    attempt graphs

--- pages_project/urls.py
+++ pages_project/urls.py
@@ -19,6 +19,6 @@ import pages.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
-    url('mplimage.png', pages.views.mplimage),
+    path('', include('pages.urls')),
+    path('mplimage.png', pages.views.mplimage),
 ]
