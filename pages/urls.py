3676f0b564fcccc88339bc30981e6aa2609be849
Dustin Squires <itsyourdustiny@gmail.com>
2019-10-11 16:13:49 -0400

    attempt graphs

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,6 @@ from .views import HomePageView, AboutPageView, IndexPageView
 urlpatterns = [
 	path('about/', AboutPageView.as_view(), name = 'about'),
 	path('', HomePageView.as_view(), name = 'home'),
-	path('index/', IndexPageView.as_view(), name = 'index')
+	path('index/', IndexPageView.as_view(), name = 'index'),
 	path('mplimage.png', pages.views.mplimage),
 ]
