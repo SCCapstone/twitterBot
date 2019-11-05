abec48065ca1976c465efe2675c0346967f93b97
ddemaria <ddemaria4@gmail.com>
2019-11-05 16:20:32 -0500

    fix static css admin page

--- tweeter_bot/settings.py
+++ tweeter_bot/settings.py
@@ -35,8 +35,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dirs'),
-    os.path.join(BASE_DIR, 'static_root'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 INSTALLED_APPS = [
