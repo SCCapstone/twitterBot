2a22cfaff6243928bb764903ccc70ef5d4932aae
ddemaria <ddemaria4@gmail.com>
2019-11-11 11:45:18 -0500

    adding back whitenoise

--- tweeter_bot/settings.py
+++ tweeter_bot/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
