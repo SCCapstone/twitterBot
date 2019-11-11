93685ba47235591f3f57c3cfef479c8ca8044d50
ddemaria <ddemaria4@gmail.com>
2019-11-11 12:42:01 -0500

    settings change

--- tweeter_bot/settings.py
+++ tweeter_bot/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static', 'static_dirs'),
