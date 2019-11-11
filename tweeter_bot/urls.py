83aa2c03a654492f0e39ac4ad24e620515215f4a
ddemaria <ddemaria4@gmail.com>
2019-11-11 12:34:21 -0500

    urls change

--- tweeter_bot/urls.py
+++ tweeter_bot/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib.auth import views
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 #import pages.views
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('accounts/', include ('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
