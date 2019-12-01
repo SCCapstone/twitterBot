28c77b6e01acdf94dd81dbfc311b320aa3ca09cd
joeyoneill <jtoneill@email.sc.edu>
2019-12-01 16:20:45 -0500

    Profile: profile view added picture (Needs further cleaning after POC)

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name = 'profile'),
     path('bokeh/', views.ResultsView.as_view(), name='bokeh'),
 	path('signup/', views.SignUp.as_view(), name='signup'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
