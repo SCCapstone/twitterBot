f68bc59f92488ec0cddc17fc56fcc46a77cdbae0
itsyourdustiny <squireso@email.sc.edu>
2020-04-22 22:29:59 -0400

    default to about page

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('about/', views.AboutView.as_view(), name = 'about'),
+    path('home/', views.HomeView.as_view(), name='home'),
+    path('', views.AboutView.as_view(), name = 'about'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
